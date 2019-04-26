8f176d1417990c1ffe5ec54600e0d02946a49116
Nils VAN ZUIJLEN <nils.van-zuijlen@mailo.com>
2019-04-26 11:03:10 +0200

    Add analog pins to the digital list.

--- pinout.h
+++ pinout.h
@@ -1,7 +1,7 @@
 #ifndef PINOUT_H
 #define PINOUT_H 1
 
-const int DIGITAL_SENSORS[]={2, 3, 4, 5, 6, 7, 8, 9, 10};
+const int DIGITAL_SENSORS[]={2, 3, 4, 5, 6, 7, 8, 9, 10, A0, A1, A2, A3, A4, A5, A6, A7};
 const int DIGITAL_SENSORS_LEN = (sizeof DIGITAL_SENSORS / sizeof DIGITAL_SENSORS[0]);
 
 #endif
