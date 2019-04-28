42691a48a154b1cfd9324885d8d46ff759d4e110
Nils VAN ZUIJLEN <nils.van-zuijlen@mailo.com>
2019-04-28 11:22:26 +0200

    Update

--- enigma-sensorboard.ino
+++ enigma-sensorboard.ino
@@ -1,23 +1,81 @@
 #include "pinout.h"
 
 void setup() {
-  Serial.begin(9600);for (int i = 0; i < DIGITAL_SENSORS_LEN; i++) {
-    pinMode(DIGITAL_SENSORS[i],INPUT);
-  }
+	Serial.begin(9600);
+
+	for (int i = 0; i < INPUTS_LEN; i++)
+		pinMode(INPUTS[i], INPUT);
+	for (int i = 0; i < OUTPUTS_LEN; i++){
+		pinMode(OUTPUTS[i], OUTPUT);
+		digitalWrite(OUTPUTS[i], OUTPUT);
+	}
+}
+
+/**
+ * Call : whois;
+ * Returns: name;
+ */
+void whois() {
+	Serial.println("SensorBoard;");
+}
+
+/**
+ * Call: dsensors;
+ * Returns: numeric value of each sensor, in the order of INPUTS ex: 00100100;
+ */
+void dsensors() {
+	for (int i = 0; i < INPUTS_LEN; i++) {
+		Serial.print(int(digitalRead(INPUTS[i])));
+	}
+	Serial.println(";");
+}
+
+/**
+ * Call: sensorX; with X the sensor number
+ * Returns: X;
+ *		X: The sensor value
+ */
+void read(int sensor) {
+	Serial.print(int(digitalRead(sensor)));
+	Serial.println(";");
+}
+
+/**
+ * Call: setoutsXXXXXXXX;
+ * 		X: numeric value of the output
+ * Returns: done;
+ */
+void outputs(String &values) {
+	for (int i = 0; i < OUTPUTS_LEN; ++i) {
+		if (values[i] == '1'){
+			digitalWrite(OUTPUTS[i], HIGH);
+		} else {
+			digitalWrite(OUTPUTS[i], LOW);
+		}
+	}
+	Serial.println("done;");
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    // read the incoming byte:
-    String message = Serial.readStringUntil(';');
-    if (message.equals("whois")) {
-      Serial.println("SensorBoard;");
-    }
-    else if (message.equals("dsensors")) {
-      for (int i = 0; i < DIGITAL_SENSORS_LEN; i++) {
-        Serial.print(int(digitalRead(DIGITAL_SENSORS[i])));
-      }
-      Serial.println(";");
-    }
-  }
+	if (Serial.available() > 0) {
+		// read the incoming byte:
+		String message = Serial.readStringUntil(';');
+
+		// test the obtained string to know the received command
+
+		if (message.equals("whois")) {
+			whois();
+		}
+		else if (message.equals("dsensors")) {
+			dsensors();
+		}
+		else if (message.startsWith("setouts")) {
+			message.remove(0, 7);
+			outputs(message);
+		}
+		else if (message.startsWith("sensor")) {
+			message.remove(0, 6);
+			read(INPUTS[message.toInt()]);
+		}
+	}
 }
--- pinout.h
+++ pinout.h
@@ -1,7 +1,34 @@
 #ifndef PINOUT_H
 #define PINOUT_H 1
 
-const int DIGITAL_SENSORS[]={2, 3, 4, 5, 6, 7, 8, 9, 10, A0, A1, A2, A3, A4, A5, A6, A7};
-const int DIGITAL_SENSORS_LEN = (sizeof DIGITAL_SENSORS / sizeof DIGITAL_SENSORS[0]);
+#define IN0  2
+#define OUT0 3
+
+#define IN1  4
+#define OUT1 5
+
+#define IN2  6
+#define OUT2 7
+
+#define IN3  8
+#define OUT3 9
+
+#define IN4  A0
+#define OUT4 A1
+
+#define IN5  A2
+#define OUT5 A3
+
+#define IN6  A4
+#define OUT6 A5
+
+#define IN7  A6
+#define OUT7 A7
+
+const int INPUTS[]={IN0, IN1, IN2, IN3, IN4, IN5, IN6, IN7};
+const int INPUTS_LEN = (sizeof INPUTS / sizeof INPUTS[0]);
+
+const int OUTPUTS[]={OUT0, OUT1, OUT2, OUT3, OUT4, OUT5, OUT6, OUT7};
+const int OUTPUTS_LEN = (sizeof OUTPUTS / sizeof OUTPUTS[0]);
 
 #endif
